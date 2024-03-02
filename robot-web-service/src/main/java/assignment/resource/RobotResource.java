0ee6d061130b20c53895443b789593ba66e88905
Carlos Tadeu Castilho Santos <csantos.everify@digio.com.br>
2024-03-02 12:41:43 -0300

    [robot-feature] adding controller

--- robot-web-service/src/main/java/assignment/resource/RobotResource.java
+++ robot-web-service/src/main/java/assignment/resource/RobotResource.java
@@ -1,12 +1,19 @@
 package assignment.resource;
 
-import assignment.request.RobotMovementRequest;
+import assignment.resource.request.RobotMovesRequest;
+import assignment.resource.request.RobotLocationsRequest;
+import assignment.resource.response.RobotLocationResponse;
+import assignment.resource.response.RobotMovesResponse;
 import assignment.service.RobotService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/locations")
+import java.util.List;
+
+@RestController
+@RequestMapping()
 public class RobotResource {
 
     private final RobotService robotService;
@@ -15,9 +22,15 @@ public class RobotResource {
         this.robotService = robotService;
     }
 
-    @PostMapping("") // This is the endpoint for the robot movement request
-    public void moveRobot(
-            @RequestBody RobotMovementRequest robotMovementRequest) {
-             robotService.moveRobot(robotMovementRequest.direction(), robotMovementRequest.steps());
+    @PostMapping("/locations") // This is the endpoint for the robot movement request
+    public List<RobotLocationResponse> movesToLocations(
+            @RequestBody List<RobotMovesRequest> requests) {
+             return robotService.movesToLocations(requests);
+    }
+
+    @PostMapping("/moves")
+    public List<RobotMovesResponse> locationsToMoves(
+            @RequestBody List<RobotLocationsRequest> requests) {
+             return robotService.locationsToMoves(requests);
     }
 }
